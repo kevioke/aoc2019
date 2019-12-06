2466b92975afed98e863e477f6e7ddcfd4c4bde1
kevin <kevin@goguardian.com>
2019-12-05 21:09:44 -0800

    Progress on problem 3

---
+++ 3.pl
@@ -0,0 +1,71 @@
+directions(Dir1, Dir2) :-
+  Dir1 = [right(997),down(443),left(406),down(393),left(66),down(223),right(135),up(452),left(918),up(354),left(985),down(402),right(257),up(225),right(298),up(369),left(762),down(373),right(781),down(935),right(363),up(952),left(174),down(529),left(127),down(549),right(874),down(993),left(890),up(881),right(549),up(537),left(174),up(766),right(244),up(131),right(861),down(487),right(849),up(304),left(653),down(497),left(711),down(916),right(12),down(753),right(19),down(528),left(944),down(155),left(507),up(552),right(844),down(822),right(341),up(948),left(922),up(866),right(387),up(973),right(534),up(127),right(48),up(744),right(950),up(522),right(930),up(320),right(254),down(577),left(142),down(29),left(24),down(118),left(583),down(683),left(643),up(974),left(683),up(985),right(692),down(271),left(279),up(62),right(157),down(932),left(556),up(574),right(615),down(428),right(296),up(551),left(452),up(533),right(475),down(302),right(39),up(846),right(527),down(433),left(453),down(567),right(614),up(807),right(463),up(712),left(247),down(436),right(141),up(180),right(783),down(65),left(379),down(935),right(989),up(945),left(901),down(160),right(356),down(828),right(45),down(619),right(655),up(104),right(37),up(793),left(360),down(242),left(137),down(45),left(671),down(844),right(112),up(627),right(976),up(10),right(942),up(26),left(470),down(284),right(832),down(59),right(97),down(9),left(320),down(38),right(326),up(317),left(752),up(213),right(840),up(789),left(152),down(64),left(628),up(326),left(640),down(610),left(769),up(183),right(844),up(834),right(342),up(630),left(945),down(807),left(270),down(472),right(369),down(920),right(283),up(440),left(597),up(137),left(133),up(458),right(266),up(91),right(137),up(536),right(861),down(325),right(902),down(971),right(891),up(648),left(573),up(139),right(951),down(671),right(996),up(864),left(749),down(681),right(255),up(306),right(154),up(706),left(817),down(798),right(109),down(594),right(496),down(867),left(217),down(572),left(166),up(723),right(66),down(210),right(732),down(741),left(21),down(574),left(523),down(646),right(313),down(961),left(474),up(990),right(125),up(928),left(58),up(726),right(200),down(364),right(244),up(622),right(823),up(39),right(918),up(549),right(667),up(935),right(372),up(241),left(56),down(713),left(735),up(735),left(812),up(700),left(408),up(980),left(242),down(697),left(580),down(34),left(266),up(190),right(876),up(857),left(967),up(493),right(871),up(563),left(241),down(636),left(467),down(793),right(304),up(103),left(950),down(503),right(487),down(868),left(358),down(747),left(338),down(273),left(485),down(686),left(974),down(724),left(534),up(561),right(729),down(162),right(731),down(17),right(305),up(712),right(472),down(158),right(921),up(827),left(944),down(303),left(526),down(782),right(575),up(948),left(401),down(142),left(48),up(766),right(799),down(242),right(821),up(673),left(120)],
+  Dir2 = [left(991),down(492),left(167),down(678),left(228),up(504),right(972),up(506),right(900),up(349),right(329),down(802),right(616),up(321),right(252),up(615),right(494),up(577),right(322),down(593),right(348),up(140),left(676),up(908),left(528),down(247),left(498),down(79),left(247),down(432),left(569),up(206),left(668),down(269),left(25),up(180),right(181),down(268),right(655),down(346),right(716),up(240),left(227),down(239),left(223),up(760),left(10),down(92),left(633),down(425),right(198),up(222),left(542),down(790),left(596),up(667),left(87),down(324),right(456),up(366),right(888),up(319),right(784),down(948),right(641),down(433),left(519),up(950),left(689),down(601),left(860),up(233),right(21),down(214),left(89),up(756),left(361),up(258),left(950),down(483),right(252),up(206),left(184),up(574),left(540),up(926),right(374),up(315),right(357),up(512),right(503),up(917),right(745),down(809),left(94),down(209),right(616),up(47),right(61),down(993),left(589),down(1),right(387),down(731),right(782),up(771),left(344),up(21),left(88),up(614),right(678),up(259),left(311),down(503),left(477),up(829),right(861),down(46),right(738),down(138),left(564),down(279),left(669),up(328),left(664),up(720),left(746),up(638),right(790),down(242),right(504),down(404),right(409),down(753),left(289),up(128),left(603),down(696),left(201),down(638),left(902),down(279),left(170),down(336),left(311),up(683),left(272),up(396),right(180),down(8),right(816),down(904),left(129),down(809),right(168),down(655),left(459),down(545),left(839),up(910),left(642),up(704),right(301),down(235),right(469),down(556),left(624),down(669),left(174),down(272),right(515),down(60),left(668),up(550),left(903),down(881),left(600),down(734),right(815),up(585),right(39),down(87),right(198),down(418),left(150),down(964),left(818),down(250),left(198),down(127),right(521),up(478),left(489),down(676),left(84),up(973),right(384),down(167),right(372),down(981),left(733),down(682),right(746),down(803),left(834),down(421),right(153),up(752),left(381),down(990),right(216),up(469),left(446),down(763),right(332),down(813),left(701),up(872),left(39),down(524),left(469),up(508),left(700),down(382),left(598),up(563),right(652),down(901),right(638),down(358),left(486),down(735),left(232),up(345),right(746),up(818),left(13),up(618),right(881),down(647),right(191),up(652),right(358),up(423),left(137),down(224),right(415),up(82),right(778),down(403),right(661),down(157),right(393),down(954),left(308),down(986),left(293),up(870),right(13),up(666),left(232),up(144),right(887),up(364),left(507),up(520),right(823),down(11),left(927),down(904),right(618),up(875),right(143),down(457),right(459),down(755),right(677),down(561),left(499),up(267),left(721),up(274),left(700),down(870),left(612),down(673),left(811),down(695),right(929),down(84),left(578),up(201),left(745),up(963),left(185),down(687),left(662),up(313),left(853),up(314),right(336)].
+
+append( [], X, X).
+append( [X | Y], Z, [X | W]) :- append( Y, Z, W).
+
+set_intersection([], _, []).
+
+set_intersection([H1|T1], L2, [H1|Res]) :-
+    member(H1, L2),
+    set_intersection(T1, L2, Res).
+
+set_intersection([_|T1], L2, Res) :-
+  set_intersection(T1, L2, Res).
+
+last_pos([X], X).
+last_pos([_|T], Val) :- last_pos(T, Val).
+
+gen(pos(_, _), up(0), []).
+gen(pos(X, Y), up(V), [pos(NewX, NewY)|Rest]) :-
+  NewX is X,
+  NewY is Y + 1,
+  NextV is V - 1,
+  gen(pos(X, NewY), up(NextV), Rest).
+
+gen(pos(_, _), down(0), []).
+gen(pos(X, Y), down(V), [pos(NewX, NewY)|Rest]) :-
+  NewX is X,
+  NewY is Y - 1,
+  NextV is V - 1,
+  gen(pos(X, NewY), down(NextV), Rest).
+
+gen(pos(_, _), left(0), []).
+gen(pos(X, Y), left(V), [pos(NewX, NewY)|Rest]) :-
+  NewX is X - 1,
+  NewY is Y,
+  NextV is V - 1,
+  gen(pos(NewX, Y), left(NextV), Rest).
+
+gen(pos(_, _), right(0), []).
+gen(pos(X, Y), right(V), [pos(NewX, NewY)|Rest]) :-
+  NewX is X + 1,
+  NewY is Y,
+  NextV is V - 1,
+  gen(pos(NewX, Y), right(NextV), Rest).
+
+gen_trail(CurrentPos, [Direction], Trail) :-
+  gen(CurrentPos, Direction, Trail).
+
+gen_trail(CurrentPos, [Direction|RemainingDirections], FullTrail) :-
+  gen(CurrentPos, Direction, CurrentTrail),
+  last_pos(CurrentTrail, NextPos),
+  gen_trail(NextPos, RemainingDirections, RemainingTrail),
+  append(CurrentTrail, RemainingTrail, FullTrail).
+
+debug_gen1(Len1, Len2) :-
+  directions(Dir1, Dir2),
+  gen_trail(pos(0,0), Dir1, Trail1),
+  gen_trail(pos(0,0), Dir2, Trail2),
+  length(Trail1, Len1),
+  length(Trail2, Len2).
+
+find_intersection(Directions1, Directions2, SetIntersection) :-
+  gen_trail(pos(0,0), Directions1, Trail1),
+  gen_trail(pos(0,0), Directions2, Trail2),
+  set_intersection(Trail1, Trail2, SetIntersection).
+
+solution1(Intersection) :-
+  directions(Dir1, Dir2),
+  find_intersection(Dir1, Dir2, Intersection).
