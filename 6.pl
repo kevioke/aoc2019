020d851bc90903d65c000eac9752396279542fa9
Kevin Nguyen <kevioke1337@gmail.com>
2019-12-09 02:03:40 -0800

    Speed up solution

--- 6.pl
+++ 6.pl
@@ -12,13 +12,16 @@ planet(X) -->
   { A \= '\n', B \= '\n', C \= '\n' },
   {atomic_list_concat([A, B, C], X)}.
 
-orbits(Orbitals, A, B) :- directly_orbits_check(Orbitals, A, B).
-orbits(Orbitals, A, B) :- directly_orbits_check(Orbitals, A, C), orbits(Orbitals, C, B).
-
-directly_orbits_check(Orbitals, A, B) :-
-  member(directly_orbits(A, B), Orbitals).
+orbits(A, B) :- directly_orbits(A, B).
+orbits(A, B) :- directly_orbits(A, C), orbits(C, B).
 
 solution1(Filename, NumTotalOrbits) :-
   parse_file(Filename, _, Orbitals),
-  setof(orbital(A, B), orbits(Orbitals, A, B), AllOrbits),
+  assert_orbits(Orbitals),
+  setof(orbital(A, B), orbits(A, B), AllOrbits),
   length(AllOrbits, NumTotalOrbits).
+
+assert_orbits([]).
+assert_orbits([Orbit|RemainingOrbits]) :-
+  assert(Orbit),
+  assert_orbits(RemainingOrbits).
