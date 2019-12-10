9fc2a73eaff36827140caf1c6479b4ba7b522734
Kevin Nguyen <kevioke1337@gmail.com>
2019-12-10 08:36:51 -0800

    Problem 7 part 1 complete

---
+++ 7.pl
@@ -0,0 +1,179 @@
+replace([_|Tail], 0, NewElem, [NewElem|Tail]).
+replace([Head|Tail], Pos, Elem, [Head|NewTail]) :- NewPos is Pos - 1, replace(Tail, NewPos, Elem, NewTail), !.
+
+mode(Int, Mode1, Mode2, Mode3):-
+  Int1 is mod(Int // 100, 10),
+  Int2 is mod(Int // 1000, 10),
+  Int3 is mod(Int // 10000, 10),
+  mode(Int1, Mode1),
+  mode(Int2, Mode2),
+  mode(Int3, Mode3).
+
+mode(0, position_mode).
+mode(1, immediate_mode).
+
+math_op(1, add).
+math_op(2, mul).
+
+do_math_op(add, Val1, Val2, Result) :-
+  Result is Val1 + Val2.
+do_math_op(mul, Val1, Val2, Result) :-
+  Result is Val1 * Val2.
+
+resolve_val(State, position_mode, Param, Val) :-
+  nth0(Param, State, Pos),
+  nth0(Pos, State, Val).
+resolve_val(State, immediate_mode, Param, Val) :-
+  nth0(Param, State, Val).
+
+%halt instruction
+run_computer(State, InstPtr, State, [State], _, []) :- nth0(InstPtr, State, 99).
+
+%add or multiply instructions
+run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
+  nth0(InstPtr, State, CurrentInt),
+  OpInt is mod(CurrentInt, 100),
+  math_op(OpInt, Op),
+
+  mode(CurrentInt, Mode1, Mode2, _),
+  ParamPtr1 is InstPtr + 1,
+  ParamPtr2 is InstPtr + 2,
+  resolve_val(State, Mode1, ParamPtr1, Val1),
+  resolve_val(State, Mode2, ParamPtr2, Val2),
+
+  do_math_op(Op, Val1, Val2, MathResult),
+
+  DestPtr is InstPtr + 3,
+  resolve_val(State, immediate_mode, DestPtr, Dest),
+  replace(State, Dest, MathResult, NextState),
+
+  NextInstPtr is InstPtr + 4,
+  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+
+%print instruction
+run_computer(State, InstPtr, FinalState, [printed(State, InstPtr)|NextStates], Inputs, [Val|Outputs]) :-
+  nth0(InstPtr, State, CurrentInt),
+  OpInt is mod(CurrentInt, 100),
+  OpInt = 4,
+  mode(CurrentInt, Mode, _, _),
+
+  ValPtr is InstPtr + 1,
+  resolve_val(State, Mode, ValPtr, Val),
+  %print(Val),
+  %nl,
+
+  NextInstPtr is InstPtr + 2,
+  run_computer(State, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+
+%read instruction
+run_computer(State, InstPtr, FinalState, [State|NextStates], [Input|Inputs], Outputs) :-
+  nth0(InstPtr, State, CurrentInt),
+  OpInt is mod(CurrentInt, 100),
+  OpInt = 3,
+
+  DestPtr is InstPtr + 1,
+  resolve_val(State, immediate_mode, DestPtr, Dest),
+  replace(State, Dest, Input, NextState), %Hard coded input!!!
+
+  NextInstPtr is InstPtr + 2,
+  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+
+run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
+  nth0(InstPtr, State, CurrentInt),
+  OpInt is mod(CurrentInt, 100),
+  OpInt = 5,
+
+  mode(CurrentInt, Mode1, Mode2, _),
+  ParamPtr1 is InstPtr + 1,
+  ParamPtr2 is InstPtr + 2,
+  resolve_val(State, Mode1, ParamPtr1, Val1),
+  resolve_val(State, Mode2, ParamPtr2, Val2),
+
+  jump_if_true(Val1, Val2, InstPtr, NextInstPtr),
+  run_computer(State, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+
+run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
+  nth0(InstPtr, State, CurrentInt),
+  OpInt is mod(CurrentInt, 100),
+  OpInt = 6,
+
+  mode(CurrentInt, Mode1, Mode2, _),
+  ParamPtr1 is InstPtr + 1,
+  ParamPtr2 is InstPtr + 2,
+  resolve_val(State, Mode1, ParamPtr1, Val1),
+  resolve_val(State, Mode2, ParamPtr2, Val2),
+
+  jump_if_false(Val1, Val2, InstPtr, NextInstPtr),
+  run_computer(State, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+
+run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
+  nth0(InstPtr, State, CurrentInt),
+  OpInt is mod(CurrentInt, 100),
+  OpInt = 7,
+
+  mode(CurrentInt, Mode1, Mode2, _),
+  ParamPtr1 is InstPtr + 1,
+  ParamPtr2 is InstPtr + 2,
+  resolve_val(State, Mode1, ParamPtr1, Val1),
+  resolve_val(State, Mode2, ParamPtr2, Val2),
+
+  less_than(Val1, Val2, Result),
+  DestPtr is InstPtr + 3,
+  resolve_val(State, immediate_mode, DestPtr, Dest),
+  replace(State, Dest, Result, NextState),
+
+  NextInstPtr is InstPtr + 4,
+  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+
+
+run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
+  nth0(InstPtr, State, CurrentInt),
+  OpInt is mod(CurrentInt, 100),
+  OpInt = 8,
+
+  mode(CurrentInt, Mode1, Mode2, _),
+  ParamPtr1 is InstPtr + 1,
+  ParamPtr2 is InstPtr + 2,
+  resolve_val(State, Mode1, ParamPtr1, Val1),
+  resolve_val(State, Mode2, ParamPtr2, Val2),
+
+  equals(Val1, Val2, Result),
+  DestPtr is InstPtr + 3,
+  resolve_val(State, immediate_mode, DestPtr, Dest),
+  replace(State, Dest, Result, NextState),
+
+  NextInstPtr is InstPtr + 4,
+  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+
+jump_if_true(Val1, Val2, _, Val2) :- Val1 \= 0.
+jump_if_true(Val1, _, InstPtr, NextInstPtr) :- Val1 = 0, NextInstPtr is InstPtr + 3.
+jump_if_false(Val1, Val2, _, Val2) :- Val1 = 0.
+jump_if_false(Val1, _, InstPtr, NextInstPtr) :- Val1 \= 0, NextInstPtr is InstPtr + 3.
+
+less_than(Val1, Val2, 1) :- Val1 < Val2.
+less_than(Val1, Val2, 0) :- Val1 >= Val2.
+
+equals(Val, Val, 1).
+equals(Val1, Val2, 0) :- Val1 \= Val2.
+
+run_amps(State, [Phase], Input, Output) :-
+  run_computer(State, 0, _, _, [Phase|Input], Output).
+
+run_amps(State, [Phase|Phases], Input, Output) :-
+  run_computer(State, 0, _, _, [Phase|Input], IntermediateOutput),
+  run_amps(State, Phases, IntermediateOutput, Output). 
+
+outputs(State, Output) :-
+  permutation([0, 1, 2, 3, 4], Phase),
+  run_amps(State, Phase, [0], Output).
+
+solution1(State, MaxOutput) :-
+  setof(Output, outputs(State, Output), OutputSet),
+  max_list(OutputSet, MaxOutput).
+
+run_amps_feedback([State|States], [Phase|Phases], Input, Output, StopPhase) :-
+  run_computer(State, 0, _, _, [Phase|Input], IntermediateOutput),
+
+run_amps_feedback([State|States], [Phase|Phases], Input, Output, StopPhase) :-
+  run_computer(State, 0, _, _, [Phase|Input], IntermediateOutput),
+  append(States, State, NewStates),
