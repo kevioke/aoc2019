a634ce95baf7769fdfdb29018fb158681180372e
kevin <kevin@goguardian.com>
2019-12-13 01:49:34 -0800

    Refactor Intcode

--- 7.pl
+++ 7.pl
@@ -1,16 +1,12 @@
 replace([_|Tail], 0, NewElem, [NewElem|Tail]).
 replace([Head|Tail], Pos, Elem, [Head|NewTail]) :- NewPos is Pos - 1, replace(Tail, NewPos, Elem, NewTail), !.
 
-mode(Int, Mode1, Mode2, Mode3):-
-  Int1 is mod(Int // 100, 10),
-  Int2 is mod(Int // 1000, 10),
-  Int3 is mod(Int // 10000, 10),
-  mode(Int1, Mode1),
-  mode(Int2, Mode2),
-  mode(Int3, Mode3).
-
-mode(0, position_mode).
-mode(1, immediate_mode).
+mode(IntInstruction, 0, position_mode) :- 0 is mod(IntInstruction, 10).
+mode(IntInstruction, 0, immediate_mode) :- 1 is mod(IntInstruction, 10).
+mode(IntInstruction, ParamIndex, Mode) :-
+  ParamIndex \= 0,
+  NewParamIndex is ParamIndex - 1,
+  mode(IntInstruction // 10, NewParamIndex, Mode).
 
 math_op(1, add).
 math_op(2, mul).
@@ -20,160 +16,232 @@ do_math_op(add, Val1, Val2, Result) :-
 do_math_op(mul, Val1, Val2, Result) :-
   Result is Val1 * Val2.
 
-resolve_val(State, position_mode, Param, Val) :-
-  nth0(Param, State, Pos),
-  nth0(Pos, State, Val).
-resolve_val(State, immediate_mode, Param, Val) :-
-  nth0(Param, State, Val).
+jump_type(5, jump_if_true).
+jump_type(6, jump_if_false).
 
-%halt instruction
-run_computer(State, InstPtr, State, [State], _, []) :- nth0(InstPtr, State, 99).
+jump(jump_if_true, Val1, Val2, _, Val2) :- Val1 \= 0.
+jump(jump_if_true, Val1, _, InstPtr, NextInstPtr) :- Val1 = 0, NextInstPtr is InstPtr + 3.
+jump(jump_if_false, Val1, Val2, _, Val2) :- Val1 = 0.
+jump(jump_if_false, Val1, _, InstPtr, NextInstPtr) :- Val1 \= 0, NextInstPtr is InstPtr + 3.
 
-%add or multiply instructions
-run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
-  nth0(InstPtr, State, CurrentInt),
-  OpInt is mod(CurrentInt, 100),
-  math_op(OpInt, Op),
+comparison_type(7, less_than).
+comparison_type(8, equals).
+comparison(less_than, Val1, Val2, 1) :- Val1 < Val2.
+comparison(less_than, Val1, Val2, 0) :- Val1 >= Val2.
+comparison(equals, Val, Val, 1).
+comparison(equals, Val1, Val2, 0) :- Val1 \= Val2.
 
-  mode(CurrentInt, Mode1, Mode2, _),
-  ParamPtr1 is InstPtr + 1,
-  ParamPtr2 is InstPtr + 2,
-  resolve_val(State, Mode1, ParamPtr1, Val1),
-  resolve_val(State, Mode2, ParamPtr2, Val2),
+resolve_val(Memory, position_mode, Param, Val) :-
+  nth0(Param, Memory, Pos),
+  nth0(Pos, Memory, Val).
+resolve_val(Memory, immediate_mode, Param, Val) :-
+  nth0(Param, Memory, Val).
 
-  do_math_op(Op, Val1, Val2, MathResult),
+inst_ptr(Comp, InstPtr) :-
+  nth0(0, Comp, InstPtr). 
 
-  DestPtr is InstPtr + 3,
-  resolve_val(State, immediate_mode, DestPtr, Dest),
-  replace(State, Dest, MathResult, NextState),
+memory(Comp, Memory) :-
+  nth0(1, Comp, Memory).
 
-  NextInstPtr is InstPtr + 4,
-  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+memory_val(Comp, Address, MemVal) :-
+  nth0(1, Comp, Memory),
+  nth0(Address, Memory, MemVal).
 
-%print instruction
-run_computer(State, InstPtr, FinalState, [printed(State, InstPtr)|NextStates], Inputs, [Val|Outputs]) :-
-  nth0(InstPtr, State, CurrentInt),
-  OpInt is mod(CurrentInt, 100),
-  OpInt = 4,
-  mode(CurrentInt, Mode, _, _),
+%assert_comp_len(OldComp, NewComp) :-
+%  memory(OldComp, OldMemory),
+%  length(OldMemory, MemoryLength),
+%  memory(NewComp, NewMemory),
+%  length(NewMemory, MemoryLength),
+%  length(OldComp, 2),
+%  length(NewComp, 2).
 
-  ValPtr is InstPtr + 1,
-  resolve_val(State, Mode, ValPtr, Val),
-  %print(Val),
-  %nl,
+inputs(Comp, Inputs) :-
+  nth0(2, Comp, Inputs).
 
-  NextInstPtr is InstPtr + 2,
-  run_computer(State, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+outputs(Comp, Outputs) :-
+  nth0(3, Comp, Outputs).
+  
+%Halt operation
+current_operation(Comp, halt) :-
+  inst_ptr(Comp, InstPtr),
+  memory_val(Comp, InstPtr, MemVal),
+  99 is mod(MemVal, 100).
 
-%read instruction
-run_computer(State, InstPtr, FinalState, [State|NextStates], [Input|Inputs], Outputs) :-
-  nth0(InstPtr, State, CurrentInt),
-  OpInt is mod(CurrentInt, 100),
-  OpInt = 3,
+%Math operation
+current_operation(Comp, math_op(MathOp, Val1, Val2, Dest)) :-
+  inst_ptr(Comp, InstPtr),
+  memory_val(Comp, InstPtr, MemVal),
+  OpInt is mod(MemVal, 100),
+  math_op(OpInt, MathOp),
 
-  DestPtr is InstPtr + 1,
-  resolve_val(State, immediate_mode, DestPtr, Dest),
-  replace(State, Dest, Input, NextState), %Hard coded input!!!
+  Param1 is InstPtr + 1,
+  Param2 is InstPtr + 2,
+  Param3 is InstPtr + 3,
 
-  NextInstPtr is InstPtr + 2,
-  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+  mode(MemVal // 100, 0, Mode1),
+  mode(MemVal // 100, 1, Mode2),
 
-run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
-  nth0(InstPtr, State, CurrentInt),
-  OpInt is mod(CurrentInt, 100),
-  OpInt = 5,
+  memory(Comp, Memory),
+  resolve_val(Memory, Mode1, Param1, Val1),
+  resolve_val(Memory, Mode2, Param2, Val2),
+  resolve_val(Memory, immediate_mode, Param3, Dest).
 
-  mode(CurrentInt, Mode1, Mode2, _),
-  ParamPtr1 is InstPtr + 1,
-  ParamPtr2 is InstPtr + 2,
-  resolve_val(State, Mode1, ParamPtr1, Val1),
-  resolve_val(State, Mode2, ParamPtr2, Val2),
+%Read operation
+current_operation(Comp, input_op(Dest)) :-
+  inst_ptr(Comp, InstPtr),
+  memory_val(Comp, InstPtr, MemVal),
+  3 is mod(MemVal, 100),
 
-  jump_if_true(Val1, Val2, InstPtr, NextInstPtr),
-  run_computer(State, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+  memory(Comp, Memory),
+  Param1 is InstPtr + 1,
+  resolve_val(Memory, immediate_mode, Param1, Dest).
 
-run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
-  nth0(InstPtr, State, CurrentInt),
-  OpInt is mod(CurrentInt, 100),
-  OpInt = 6,
+%Output operation
+current_operation(Comp, output_op(Address)) :-
+  inst_ptr(Comp, InstPtr),
+  memory_val(Comp, InstPtr, MemVal),
+  4 is mod(MemVal, 100),
 
-  mode(CurrentInt, Mode1, Mode2, _),
-  ParamPtr1 is InstPtr + 1,
-  ParamPtr2 is InstPtr + 2,
-  resolve_val(State, Mode1, ParamPtr1, Val1),
-  resolve_val(State, Mode2, ParamPtr2, Val2),
+  memory(Comp, Memory),
+  Param1 is InstPtr + 1,
+  resolve_val(Memory, immediate_mode, Param1, Address).
 
-  jump_if_false(Val1, Val2, InstPtr, NextInstPtr),
-  run_computer(State, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+%Jump operation
+current_operation(Comp, jump_op(NewInstPtr)) :-
+  inst_ptr(Comp, InstPtr),
+  memory_val(Comp, InstPtr, MemVal),
+  OpInt is mod(MemVal, 100),
+  jump_type(OpInt, JumpType),
 
-run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
-  nth0(InstPtr, State, CurrentInt),
-  OpInt is mod(CurrentInt, 100),
-  OpInt = 7,
+  mode(MemVal // 100, 0, Mode1),
+  mode(MemVal // 100, 1, Mode2),
+  Param1 is InstPtr + 1,
+  Param2 is InstPtr + 2,
 
-  mode(CurrentInt, Mode1, Mode2, _),
-  ParamPtr1 is InstPtr + 1,
-  ParamPtr2 is InstPtr + 2,
-  resolve_val(State, Mode1, ParamPtr1, Val1),
-  resolve_val(State, Mode2, ParamPtr2, Val2),
+  memory(Comp, Memory),
+  resolve_val(Memory, Mode1, Param1, Val1),
+  resolve_val(Memory, Mode2, Param2, Val2),
+  jump(JumpType, Val1, Val2, InstPtr, NewInstPtr).
 
-  less_than(Val1, Val2, Result),
-  DestPtr is InstPtr + 3,
-  resolve_val(State, immediate_mode, DestPtr, Dest),
-  replace(State, Dest, Result, NextState),
+%Comparison operation
+current_operation(Comp, comparison_op(Result, Dest)) :-
+  inst_ptr(Comp, InstPtr),
+  memory_val(Comp, InstPtr, MemVal),
+  OpInt is mod(MemVal, 100),
+  comparison_type(OpInt, ComparisonType),
 
-  NextInstPtr is InstPtr + 4,
-  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+  mode(MemVal // 100, 0, Mode1),
+  mode(MemVal // 100, 1, Mode2),
+  Param1 is InstPtr + 1,
+  Param2 is InstPtr + 2,
+  Param3 is InstPtr + 3,
 
+  memory(Comp, Memory),
+  resolve_val(Memory, Mode1, Param1, Val1),
+  resolve_val(Memory, Mode2, Param2, Val2),
+  resolve_val(Memory, immediate_mode, Param3, Dest),
+  comparison(ComparisonType, Val1, Val2, Result).
 
-run_computer(State, InstPtr, FinalState, [State|NextStates], Inputs, Outputs) :-
-  nth0(InstPtr, State, CurrentInt),
-  OpInt is mod(CurrentInt, 100),
-  OpInt = 8,
+%halt
+single_step(Comp, Comp) :- current_operation(Comp, halt).
 
-  mode(CurrentInt, Mode1, Mode2, _),
-  ParamPtr1 is InstPtr + 1,
-  ParamPtr2 is InstPtr + 2,
-  resolve_val(State, Mode1, ParamPtr1, Val1),
-  resolve_val(State, Mode2, ParamPtr2, Val2),
+%add or multiply
+single_step(Comp, NewComp) :- 
+  current_operation(Comp, math_op(Op, Val1, Val2, Dest)),
+  do_math_op(Op, Val1, Val2, Result),
 
-  equals(Val1, Val2, Result),
-  DestPtr is InstPtr + 3,
-  resolve_val(State, immediate_mode, DestPtr, Dest),
-  replace(State, Dest, Result, NextState),
+  memory(Comp, Memory),
+  replace(Memory, Dest, Result, NewMemory),
+  memory(NewComp, NewMemory),
 
-  NextInstPtr is InstPtr + 4,
-  run_computer(NextState, NextInstPtr, FinalState, NextStates, Inputs, Outputs).
+  inputs(Comp, Inputs),
+  inputs(NewComp, Inputs),
+  outputs(Comp, Outputs),
+  outputs(NewComp, Outputs),
 
-jump_if_true(Val1, Val2, _, Val2) :- Val1 \= 0.
-jump_if_true(Val1, _, InstPtr, NextInstPtr) :- Val1 = 0, NextInstPtr is InstPtr + 3.
-jump_if_false(Val1, Val2, _, Val2) :- Val1 = 0.
-jump_if_false(Val1, _, InstPtr, NextInstPtr) :- Val1 \= 0, NextInstPtr is InstPtr + 3.
+  inst_ptr(Comp, InstPtr),
+  NewInstPtr is InstPtr + 4,
+  inst_ptr(NewComp, NewInstPtr).
 
-less_than(Val1, Val2, 1) :- Val1 < Val2.
-less_than(Val1, Val2, 0) :- Val1 >= Val2.
+%input
+single_step(Comp, NewComp) :- 
+  current_operation(Comp, input_op(Dest)),
+  inputs(Comp, [Input|RemainingInputs]),
+  inputs(NewComp, RemainingInputs),
 
-equals(Val, Val, 1).
-equals(Val1, Val2, 0) :- Val1 \= Val2.
+  memory(Comp, Memory),
+  replace(Memory, Dest, Input, NewMemory),
+  memory(NewComp, NewMemory),
 
-run_amps(State, [Phase], Input, Output) :-
-  run_computer(State, 0, _, _, [Phase|Input], Output).
+  outputs(Comp, Outputs),
+  outputs(NewComp, Outputs),
 
-run_amps(State, [Phase|Phases], Input, Output) :-
-  run_computer(State, 0, _, _, [Phase|Input], IntermediateOutput),
-  run_amps(State, Phases, IntermediateOutput, Output). 
+  inst_ptr(Comp, InstPtr),
+  NewInstPtr is InstPtr + 2,
+  inst_ptr(NewComp, NewInstPtr).
 
-outputs(State, Output) :-
-  permutation([0, 1, 2, 3, 4], Phase),
-  run_amps(State, Phase, [0], Output).
+%output
+single_step(Comp, NewComp) :- 
+  current_operation(Comp, output_op(Address)),
+  memory(Comp, Memory),
+  memory(NewComp, Memory),
+  inputs(Comp, Inputs),
+  inputs(NewComp, Inputs),
 
-solution1(State, MaxOutput) :-
-  setof(Output, outputs(State, Output), OutputSet),
-  max_list(OutputSet, MaxOutput).
+  outputs(Comp, CurrentOutputs),
+  memory_val(Comp, Address, Val),
+  outputs(NewComp, [Val|CurrentOutputs]),
 
-run_amps_feedback([State|States], [Phase|Phases], Input, Output, StopPhase) :-
-  run_computer(State, 0, _, _, [Phase|Input], IntermediateOutput),
+  inst_ptr(Comp, InstPtr),
+  NewInstPtr is InstPtr + 2,
+  inst_ptr(NewComp, NewInstPtr).
 
-run_amps_feedback([State|States], [Phase|Phases], Input, Output, StopPhase) :-
-  run_computer(State, 0, _, _, [Phase|Input], IntermediateOutput),
-  append(States, State, NewStates),
+%jump
+single_step(Comp, NewComp) :-
+  current_operation(Comp, jump_op(JumpTo)),
+  memory(Comp, Memory),
+  memory(NewComp, Memory),
+  inputs(Comp, Inputs),
+  inputs(NewComp, Inputs),
+  outputs(Comp, Outputs),
+  outputs(NewComp, Outputs),
+  inst_ptr(NewComp, JumpTo).
+
+%comparison
+single_step(Comp, NewComp) :-
+  current_operation(Comp, comparison_op(Result, Dest)),
+
+  memory(Comp, Memory),
+  replace(Memory, Dest, Result, NewMemory),
+  memory(NewComp, NewMemory),
+
+  inputs(Comp, Inputs),
+  inputs(NewComp, Inputs),
+  outputs(Comp, Outputs),
+  outputs(NewComp, Outputs),
+
+  inst_ptr(Comp, InstPtr),
+  NewInstPtr is InstPtr + 4,
+  inst_ptr(NewComp, NewInstPtr).
+
+run_until_halt(Comp, Comp) :- current_operation(Comp, halt).
+run_until_halt(Comp, FinalComp) :-
+  current_operation(Comp, Op),
+  Op \= halt,
+  single_step(Comp, NextComp),
+  run_until_halt(NextComp, FinalComp).
+
+%run_amps(State, [Phase], Input, Output) :-
+%  single_step(State, 0, _, _, [Phase|Input], Output).
+%
+%run_amps(State, [Phase|Phases], Input, Output) :-
+%  single_step(State, 0, _, _, [Phase|Input], IntermediateOutput),
+%  run_amps(State, Phases, IntermediateOutput, Output). 
+%
+%outputs(State, Output) :-
+%  permutation([0, 1, 2, 3, 4], Phase),
+%  run_amps(State, Phase, [0], Output).
+%
+%solution1(State, MaxOutput) :-
+%  setof(Output, outputs(State, Output), OutputSet),
+%  max_list(OutputSet, MaxOutput).
